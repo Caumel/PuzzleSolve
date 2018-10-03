d1d2a4e56f23df8c936ddbad9dc600aa86bc8a6c
Luis Caumel <luiscaumel@gmail.com>
2018-10-03 12:11:56 +0200

    Created proyect

---
+++ src/main/kotlin/com/kotlin/autentia/AutentiaApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.autentia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutentiaApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutentiaApplication>(*args)
+}
